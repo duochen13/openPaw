5a95eaa48ed8d047b6c91cc6b406dad12ebdccd4
Duo Chen <duochen@umich.edu>
2026-05-31 01:14:50 -0700

    feat: initialize project structure and dependencies

---
+++ daily-report/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js'
+  ],
+  testMatch: [
+    '**/tests/**/*.test.js'
+  ],
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true
+};
